17e2d7c04a2b63c6b8b2cff52f4a064baceef7f1
Pushkar Singh <94926273+pushkarsingh019@users.noreply.github.com>
2022-06-28 14:53:19 +0530

    Home Screen Done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Sophia-client-website
-Created with CodeSandbox
+# Sophia Yuna Cheong
+
+This is the webiste I created as a freelance project
---
+++ src/data/reviews.js
@@ -0,0 +1,25 @@
+const reviews = [
+  {
+    name: "Izzy Sunday",
+    title: "YouTuber",
+    review:
+      "The community has been the best way to connect with content creators all over the world. I've continued to grow so much.",
+    imageSource: "https://i.imgur.com/7X6A3ek.png"
+  },
+  {
+    name: "Liana",
+    title: "Content Creator",
+    review:
+      "I'm so glad I joined this community, being able to connect with other content creators has helped fuel my passion to create!",
+    imageSource: "https://i.imgur.com/6QAc66w.png"
+  },
+  {
+    name: "Cameron",
+    title: "Fitness YouTuber",
+    review:
+      "The community has given me just that bit of extra motivation to keep going with creating content",
+    imageSource: "https://i.imgur.com/0IXPAEw.jpg"
+  }
+];
+
+export default reviews;
